bfe5456e1f9a1acfab69f156cab9548f21457423
Bart Webber <bart@aseenrichment.com>
2017-07-24 07:47:20 -0700

    add tags and list objectives to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -9,46 +9,23 @@ Pizza.prototype.pizzaPrice = function(){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // UI Logic
 $(document).ready(function(){
   $("form#pizza-form").submit(function(event){
     event.preventDefault();
     var pizzaSize = $("#pizza-size option:selected").text();
-
-    alert(pizzaSize);
     var pizzaSizeValue = parseInt($("#pizza-size").val());
     var userToppingScore = 0;
-    var toppingScore = parseInt(userToppingScore);
-
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
       userToppingScore = userToppingScore + 1;
     });
-    alert(userToppingScore);
+    var toppingScore = parseInt(userToppingScore);
 
     var newPizza = new Pizza(pizzaSizeValue, toppingScore);
 
     $(".pizza-size").text(pizzaSize);
     $(".pizza-toppings").text(userToppingScore);
     $(".pizza-price").text("$" + newPizza.pizzaPrice() + ".00");
-
     $("#show-pizza").show();
-
-
-
-
   });
 });
